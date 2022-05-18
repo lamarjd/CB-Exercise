954f798ed33df8f2df148f23c1adc4f493467378
jaggerhansen <jagger_hansen@live.com>
2022-05-18 13:39:58 -0700

    number 5 is done

--- problems/05-selective-map.js
+++ problems/05-selective-map.js
@@ -1,16 +1,11 @@
 /*******************************************************************************
-Write a function `selectiveMap` that accepts an array and two callbacks as arguments.
-The function should return a new array where elements are replaced with the results
-of calling the second callback on the element only if calling the first callback
-on the element results in true. If calling the first callback on an element results
-in false, then the element should not be changed in the new array.
 
-Note that that you cannot use the Array `map` or `filter` methods to solve this
-problem.
+ Note that that you cannot use the Array `map` or `filter` methods to solve this
+ problem.
 
-Examples:
+ Examples:
 
-function isEven(n) {
+ function isEven(n) {
     return n % 2 === 0;
 }
 
@@ -36,18 +31,62 @@ console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
 // [-10, 16, 49, 36, -2, -9]
 
 AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
-    (optional if you already asked a question for this problem.)
+(optional if you already asked a question for this problem.)
 - Explain how you are using both of the callbacks in the function.
 - What do you expect each callback function to be returning?
 - How many times are you calling each callback function?
+
+
+
+
+Write a function `selectiveMap` that accepts an array and two callbacks as arguments.
+The function should return a new array where elements are replaced with the results
+of calling the second callback on the element only if calling the first callback
+on the element results in true. If calling the first callback on an element results
+in false, then the element should not be changed in the new array.
 *******************************************************************************/
 
-let selectiveMap = function() {
-
+let selectiveMap = function (array, cb1, cb2) {
+    let newArr = []; // made new array.
+    for (let i = 0; i < array.length; i++) { // ittrate throught the array for length
+        let ele = array[i]; // set varible for each index
+        if (cb1(ele) === true) { // if the ele passed through cb1 is true.
+            newArr.push(cb2(ele)); // push the value of the ele passed through the cb2 to new array.
+        } else { // if not true,
+            newArr.push(ele); // only push ele.
+        }
+    }
+    return newArr;
 };
 
 
 
+
+
+function isEven(n) {
+    return n % 2 === 0;
+}
+
+function isPositive(n) {
+    return n > 0;
+}
+
+function square(n) {
+    return n * n;
+}
+
+function flipSign(n) {
+    return n * -1;
+}
+
+console.log(selectiveMap([8, 5, 10, 4], isEven, square));
+// [ 64, 5, 100, 16 ]
+
+console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isEven, flipSign));
+// [ 10, -4, 7, -6, 2, -9 ]
+
+console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
+// [-10, 16, 49, 36, -2, -9]
 
 
 
