e1ae84afba160f628d0cbe48ac1547b1c7846a94
jaggerhansen <jagger_hansen@live.com>
2022-05-18 15:54:46 -0700

    number 11 done

--- problems/11-and-select.js
+++ problems/11-and-select.js
@@ -1,9 +1,47 @@
 /*******************************************************************************
-Write a function `andSelect` that accepts an array and two callbacks. The function
-should return a new array containing elements of the original array that result in
-true when passed into both callbacks.
 
-Examples:
+ Examples:
+
+ let isEven = function (n) {
+     return n % 2 === 0;
+    };
+
+    let isPositive = function (n) {
+        return n > 0;
+    };
+
+    console.log(andSelect([-3, 8, 7, 11, 6, 12, -4], isEven, isPositive));
+    // [ 8, 6, 12 ]
+
+    let isUpperCase = function (s) {
+        return s === s.toUpperCase();
+    };
+
+    let startsWithA = function (s) {
+        return s[0].toUpperCase() === 'A';
+    }
+    console.log(andSelect(['ants', 'APPLES', 'ART', 'BACON', 'arm'], isUpperCase,  startsWithA));
+    // [ 'APPLES', 'ART' ]
+
+
+    Write a function `andSelect` that accepts an array and two callbacks. The function
+    should return a new array containing elements of the original array that result in
+    true when passed into both callbacks.
+
+*******************************************************************************/
+
+let andSelect = function (arr, cb1, cb2) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let ele = arr[i];
+
+        if ((cb1(ele) !== false) && (cb2(ele) !== false)) {
+            newArr.push(ele);
+        }
+    }
+    return newArr;
+};
+
 
 let isEven = function (n) {
     return n % 2 === 0;
@@ -23,17 +61,8 @@ let isUpperCase = function (s) {
 let startsWithA = function (s) {
     return s[0].toUpperCase() === 'A';
 }
-console.log(andSelect(['ants', 'APPLES', 'ART', 'BACON', 'arm'], isUpperCase,  startsWithA));
+console.log(andSelect(['ants', 'APPLES', 'ART', 'BACON', 'arm'], isUpperCase, startsWithA));
 // [ 'APPLES', 'ART' ]
-
-*******************************************************************************/
-
-let andSelect = function() {
-
-};
-
-
-
 
 
 
