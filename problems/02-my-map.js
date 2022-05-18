313b6fbedc27688cf90cd37e65be88ab831c2455
Jacob <jacob.lamar16@gmail.com>
2022-05-18 12:52:55 -0700

    Second done

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -16,11 +16,22 @@ let result2 = myMap(['run', 'Forrest'], function (el) {
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 *******************************************************************************/
 
-let myMap = function() {
-
+let myMap = function(array, cb) {
+    let newArr = []; // create a new array
+    for (let i = 0; i < array.length; i++) { // iterate the array
+        let ele = array[i]; // set value for the current index of the array as it iterates
+        newArr.push(cb(ele)); // push the element through the cb, and into the new array
+    }
+    return newArr; // return the new array
 };
 
+let result1 = myMap([100, 25, 81, 64], Math.sqrt);
+console.log(result1);   // [ 10, 5, 9, 8 ]
 
+// let result2 = myMap(['run', 'Forrest'], function (el) {
+//     return el.toUpperCase() + '!';
+// });
+// console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 
 
